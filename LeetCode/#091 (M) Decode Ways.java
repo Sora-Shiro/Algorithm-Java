cc7781b052707c71dc83952a692682847cba1700
sorashiro <sora95shiro@gmail.com>
2017-04-11 21:09:34 +0800

    2017.4.11

---
+++ LeetCode/#091 (M) Decode Ways.java
@@ -0,0 +1,68 @@
+/*
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+ */
+
+public class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        
+        int[] memo = new int[n+1];
+        memo[n]  = 1;
+        memo[n-1] = s.charAt(n-1) != '0' ? 1 : 0;
+        
+        for (int i = n - 2; i >= 0; i--)
+            if (s.charAt(i) == '0') continue;
+            else memo[i] = (Integer.parseInt(s.substring(i,i+2))<=26) ? memo[i+1]+memo[i+2] : memo[i+1];
+        
+        return memo[0];
+    }
+}
+
+public class Solution_another {
+    public int numDecodings(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) != '0' ? 1 : 0;
+        for(int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i-1, i));
+            int second = Integer.valueOf(s.substring(i-2, i));
+            if(first >= 1 && first <= 9) {
+               dp[i] += dp[i-1];  
+            }
+            if(second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
+
+public class Solution_faster {
+    public int numDecodings(String s) {
+        if(s.length() == 0) return 0;
+        int pre = 27, digit, answer = 0, first = 1, second = 1;
+        for(int i = s.length()-1; i >= 0; i--){
+            digit = s.charAt(i) - '0';
+            if(digit == 0) answer = 0;
+            else answer = first + (digit*10 + pre < 27 ? second : 0);
+            second = first; first = answer; pre = digit;
+        }
+        return answer;
+    }
+}
